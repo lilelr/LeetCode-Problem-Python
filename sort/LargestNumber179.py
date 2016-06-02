622112a1b29cc95587ad8ffeaf034c7ef1a88dad
lilelr <lilelr@163.com>
2016-06-02 15:14:18 +0800

    largestNumber 179 cmp sorted

---
+++ sort/LargestNumber179.py
@@ -0,0 +1,4 @@
+class Solution:
+    def largestNumber(self, nums):
+        ret = ''.join(sorted([str(x) for x in nums], cmp=lambda x, y: cmp(y + x, x + y)))
+        return ret if ret[0] != '0' else "0"
