efe43526d694e912fd47ecc8659135a9e4301400
lilelr <lilelr@163.com>
2016-06-02 15:50:08 +0800

    350 ac  2

--- sort/IntersectionofTwoArraysII350.py
+++ sort/IntersectionofTwoArraysII350.py
@@ -13,7 +13,15 @@ class Solution(object):
                 dict1[val] += 1
         ret = []
         for val in nums2:
-            if val in dict1 and dict1[val] >0:
+            if val in dict1 and dict1[val] > 0:
                 ret.append(val)
-                dict1[val] -=1
+                dict1[val] -= 1
         return ret
+
+    def intersect2(self, nums1, nums2):
+        result = []
+        for i in range(len(nums1)):
+            if nums1[i] in nums2:
+                nums2.remove(nums1[i])
+                result.append(nums1[i])
+        return result
